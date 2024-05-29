8b9ca825f815e621e59414ccd0805b4847595449
Mateo Montenegro <emontenegroc@est.ups.edu.ec>
2024-05-29 02:05:00 -0500

    Actualización base de datos

--- bbdd_examen.sql
+++ bbdd_examen.sql
@@ -27,4 +27,26 @@ CREATE DATABASE bd_donaciones
 	INSERT INTO tb_usuarios VALUES(1, 1, '1725578775', 'Mateo Montenegro', 'emontenegro@gmail.com', 'emontenegro', '1234');
 	SELECT * FROM tb_usuarios;
 	
+CREATE TABLE tb_fundaciones (
+    id_fu SERIAL PRIMARY KEY,
+    nombre_fu VARCHAR(100) NOT NULL,
+    telefono_fu VARCHAR(10) NOT NULL,
+	direccion_fu VARCHAR(100) UNIQUE NOT NULL,
+	representante_fu VARCHAR(100) NOT NULL);
 	
+CREATE TABLE tb_alimentos(
+	id_al SERIAL PRIMARY KEY,
+	nombre_al VARCHAR(100) NOT NULL,
+	descripcion_al VARCHAR(100) NOT NULL);
+	
+CREATE TABLE tb_fun_al(
+	id_fun_al SERIAL PRIMARY KEY,
+	id_fu INT NOT NULL,
+	id_al INT NOT NULL, 
+	cantidad_al INT NOT NULL,
+	CONSTRAINT fk_tb_fundaciones FOREIGN KEY (id_fu) REFERENCES tb_fundaciones(id_fu),
+	CONSTRAINT fk_tb_alimentos FOREIGN KEY (id_al) REFERENCES t		b_alimentos(id_al));
+
+SELECT * FROM tb_fundaciones;
+SELECT * FROM tb_usuarios;
+INSERT INTO tb_fundaciones VALUES(1, 'Ejemplo', '0987655417', 'Dirección 1', 'Juan Donoso')
